9ddd6987b05c7903bb24863eca60197b424d79a1
MBaccount <manoahberal@live.fr>
2021-10-13 17:17:20 +0200

    Revert "Push"
    This reverts commit 780dbbc44254f8172b188737d037df19751cecf9.

--- appel_subprocess.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-
-subprocess.call(["bash","-c","source ~/.profile; python3 peripheriques.py"])
--- try_pi_camera.py
+++
@@ -1,8 +0,0 @@
-import picamera
-from time import sleep
-
-camera = picamera.PiCamera()
-
-camera.capture('/home/pi/image1.jpg')
-sleep(5)
-camera.capture('/home/pi/image2.jpg')
